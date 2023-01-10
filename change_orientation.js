75a61745f04cbff2e70131f0b4addd9f3e3fb6a1
f4l5j <78499844+f4l5j@users.noreply.github.com>
2023-01-10 22:24:49 +0100

    Delete change_orientation.js

--- change_orientation.js
+++
@@ -1,4 +0,0 @@
-function changeorientation() {
-    document.body.requestFullscreen().then(res=>console.log(res).catch(err=>console.log(err)));
-    screen.orientation.lock('landscape').then(res=>console.log(res)).catch(err=>console.log(err))
-  }
